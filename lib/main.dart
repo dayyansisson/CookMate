7b5566ed437239efd65199eec612b8adce48c571
Dayyan <dayyansisson19@gmail.com>
2020-04-02 19:23:43 -0700

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(CookMate());
+
+class CookMate extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    return MaterialApp(
+      title: 'CookMate',
+    );
+  }
+}
