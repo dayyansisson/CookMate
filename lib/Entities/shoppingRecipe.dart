a9964eccc96f60ddf4d122099d79f48d29c678cd
Gabe <gmbeal@ucsd.edu>
2020-04-12 12:07:04 -0700

    shoppingRecipe updated

---
+++ lib/Entities/shoppingRecipe.dart
@@ -0,0 +1,16 @@
+import 'package:CookMate/Entities/ingredient.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:CookMate/Entities/shoppingIngredient.dart';
+
+/*
+  This file lays out the shopping ingredient class. 
+*/
+
+class ShoppingRecipe {
+  bool purchased;
+  List<ShoppingIngredient> ingredient;
+
+  //Everything from the server below
+
+}
