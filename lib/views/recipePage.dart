d1ae5c8660f256d633f910bd0858cd3b22b5eff7
Dayyan <dayyansisson19@gmail.com>
2020-04-19 12:03:12 -0700

    Finished hardcoded recipe view

--- lib/views/recipePage.dart
+++ lib/views/recipePage.dart
@@ -1,7 +1,9 @@
 import 'dart:ui';
-
+import 'package:CookMate/provider/tabNavigationModel.dart';
 import 'package:CookMate/util/styleSheet.dart';
+import 'package:CookMate/widgets/page%20layout/recipeSheet.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class RecipePage extends StatelessWidget {
 
@@ -9,7 +11,7 @@ class RecipePage extends StatelessWidget {
   static const double _PAGE_NAME_FONT_SIZE = 16;
   static const double _TOP_BAR_EDGE_PADDING = 30;
 
-  final String _imageURL = "https://www.traderjoes.com/TJ_CMS_Content/Images/Recipe/carnitas-bowl.jpg";
+  final String _imageURL = "https://ethosfun.com/wp-content/uploads/2019/09/bowl-cuisine-delicious-2067473.jpg";
   
   @override
   Widget build(BuildContext context) {
@@ -25,12 +27,12 @@ class RecipePage extends StatelessWidget {
             )
           ),
         ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        Stack(
           children: <Widget>[
             Padding(  // Top Bar
               padding: const EdgeInsets.only(top: _TOP_BAR_EDGE_PADDING, right: _TOP_BAR_EDGE_PADDING, left: _TOP_BAR_EDGE_PADDING),
               child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Text(
                     "BACK",
@@ -45,8 +47,10 @@ class RecipePage extends StatelessWidget {
                 ]
               ),
             ),
-            Spacer(),
-            RecipeTab()
+            ChangeNotifierProvider(
+              create: (_) => TabNavigationModel(tabCount: 2),
+              child: RecipeSheet()
+            )
           ],
         )
       ],
@@ -64,147 +68,6 @@ class RecipePage extends StatelessWidget {
           Container(height: 2, color: StyleSheet.WHITE),
         ],
       ),
-    );
-  }
-}
-
-class RecipeTab extends StatefulWidget {
-
-  @override
-  _RecipeTabState createState() => _RecipeTabState();
-}
-
-class _RecipeTabState extends State<RecipeTab> {
-
-  /* Layout Constants */
-  static const double _SHEET_BORDER_RADIUS = 40;
-  static const double _TITLE_SIZE = 36;
-
-  @override
-  Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.only(
-        topLeft: Radius.circular(_SHEET_BORDER_RADIUS),
-        topRight: Radius.circular(_SHEET_BORDER_RADIUS)
-      ),
-      child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
-        child: Container(
-          color: StyleSheet.BLACK.withOpacity(0.4),
-          child: Stack(
-            children: <Widget> [
-              _header,
-            ]
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget get _header {
-    return Column(
-      children: <Widget> [
-        _title,
-        SizedBox(height: 14),
-        _description,
-        _horizontalInfoBar
-      ]
-    );
-  }
-
-  /* Getter for the header segment of the sheet */
-  Widget get _title {
-    
-    return Stack(
-      alignment: Alignment.topCenter,
-      children: <Widget>[
-        Container(    // Rounded edges
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(_SHEET_BORDER_RADIUS),
-              topRight: Radius.circular(_SHEET_BORDER_RADIUS)
-            )
-          ),
-          height: _SHEET_BORDER_RADIUS + _TITLE_SIZE,
-        ),
-        Positioned(
-          top: 14,
-          child: Container(
-            width: 40,
-            height: 6,
-            decoration: BoxDecoration(
-              color: StyleSheet.WHITE.withOpacity(0.1),
-              borderRadius: BorderRadius.all(Radius.circular(_SHEET_BORDER_RADIUS))
-            ),
-          ),
-        ),
-        Positioned(  // Navigation
-          top: _SHEET_BORDER_RADIUS,
-          left: _SHEET_BORDER_RADIUS,
-          child: Text(
-            "Carnitas Bowl",
-            style: TextStyle(
-              fontSize: _TITLE_SIZE,
-              fontFamily: 'Hoefler',
-              color: StyleSheet.WHITE,
-            ),
-          )
-        ),
-      ],
-    );
-  }
-
-  Widget get _description {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: _SHEET_BORDER_RADIUS),
-        child: Text(
-          "Wild rice’s nutty and earthy flavors elevate an otherwise boring dish instantly.",
-          style: TextStyle(
-            fontSize: 20,
-            color: StyleSheet.WHITE,
-            fontWeight: FontWeight.w300
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget get _horizontalInfoBar {
-
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: _SHEET_BORDER_RADIUS, vertical: 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          _basicInfo("Prep", "15 min"),
-          _basicInfo("Cook", "10 min"),
-          _basicInfo("Serves", "4-6")
-        ],
-      ),
-    );
-  }
-
-  Widget _basicInfo(String title, String data) {
-
-    return Column(
-      children: <Widget> [
-        Text(
-          title.toUpperCase(),
-          style: TextStyle(
-            color: StyleSheet.WHITE,
-            fontSize: 15
-          ),
-        ),
-        Text(
-          data.toUpperCase(),
-          style: TextStyle(
-            color: StyleSheet.WHITE,
-            fontSize: 14,
-            fontWeight: FontWeight.w300
-          ),
-        ),
-      ]
     );
   }
 }
---
+++ lib/widgets/page layout/recipeSheet.dart
@@ -0,0 +1,381 @@
+import 'dart:ui';
+
+import 'package:CookMate/provider/tabNavigationModel.dart';
+import 'package:CookMate/util/styleSheet.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class RecipeSheet extends StatefulWidget {
+
+  @override
+  _RecipeSheetState createState() => _RecipeSheetState();
+}
+
+class _RecipeSheetState extends State<RecipeSheet> with SingleTickerProviderStateMixin {
+
+  /* Layout Constants */
+  static const double _SHEET_BORDER_RADIUS = 40;
+  static const double _TITLE_SIZE = 36;
+  static const double _DESCRIPTION_SIZE = 20;
+  static const double _INFO_SIZE = 15;
+  static const double _LINE_SPACING = 1.2;
+  static const double _HEAD_SPACE = 70;
+
+  static const double _MIN_SHEET_HEIGHT = _SHEET_BORDER_RADIUS + _TITLE_SIZE + 14 + (_DESCRIPTION_SIZE * _LINE_SPACING * 3) + 40 + (_INFO_SIZE * 2) + _HEAD_SPACE;
+
+  /* Animation Controller */
+  AnimationController dragController;
+  Animation<double> dragPosition;
+
+  @override
+  void initState() { 
+
+    super.initState();
+    
+    dragController = AnimationController(
+      vsync: this, 
+      duration: Duration(seconds: 1),
+    )..addListener(() => setState((){}));
+    
+    dragPosition = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        curve: Curves.easeInOut,
+        parent: dragController,
+      )
+    );
+
+    dragController.value = 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Padding(
+      padding: const EdgeInsets.only(top: _HEAD_SPACE),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            width: MediaQuery.of(context).size.width,
+            top: (MediaQuery.of(context).size.height - _MIN_SHEET_HEIGHT) * dragPosition.value,
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(_SHEET_BORDER_RADIUS),
+                  topRight: Radius.circular(_SHEET_BORDER_RADIUS)
+                ),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 25, sigmaY: 25),
+                  child: Container(
+                    color: Colors.white12,
+                    child: Column(
+                      children: <Widget> [
+                        _header,
+                        SizedBox(
+                          height: 60 - 60 * (1 - dragPosition.value),
+                        ),
+                        Expanded(child: tabBody),
+                      ]
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget get _header {
+    return GestureDetector(
+      onVerticalDragUpdate: (details) {
+        setState(() {
+          dragController.value += (details.primaryDelta / (MediaQuery.of(context).size.height - _MIN_SHEET_HEIGHT));
+        });
+      },
+      onVerticalDragEnd: (details) {
+        if(dragController.value > 0.5) {
+          dragController.forward(from: dragController.value);
+        } else {
+          dragController.reverse(from: dragController.value);
+        }
+      },
+      child: Button(
+        onPressed: null,
+        child: Column(
+          children: <Widget> [
+            _title,
+            SizedBox(height: 14),
+            _description,
+            _horizontalInfoBar
+          ]
+        ),
+      ),
+    );
+  }
+
+  /* Getter for the header segment of the sheet */
+  Widget get _title {
+    
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: <Widget>[
+        Container(    // Rounded edges
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(_SHEET_BORDER_RADIUS),
+              topRight: Radius.circular(_SHEET_BORDER_RADIUS)
+            )
+          ),
+          height: _SHEET_BORDER_RADIUS + _TITLE_SIZE,
+        ),
+        Positioned(
+          top: 14,
+          child: Container(
+            width: 40,
+            height: 6,
+            decoration: BoxDecoration(
+              color: StyleSheet.WHITE.withOpacity(0.1),
+              borderRadius: BorderRadius.all(Radius.circular(_SHEET_BORDER_RADIUS))
+            ),
+          ),
+        ),
+        Positioned(
+          top: _SHEET_BORDER_RADIUS,
+          left: _SHEET_BORDER_RADIUS,
+          child: Text(
+            "Wild Rice & Egg",
+            style: TextStyle(
+              fontSize: _TITLE_SIZE,
+              fontFamily: 'Hoefler',
+              color: StyleSheet.WHITE,
+            ),
+          )
+        ),
+      ],
+    );
+  }
+
+  Widget get _description {
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: _SHEET_BORDER_RADIUS),
+      child: Text(
+        "Wild rice’s nutty and earthy flavors elevate an otherwise boring dish instantly.",
+        style: TextStyle(
+          fontSize: _DESCRIPTION_SIZE,
+          height: _LINE_SPACING,
+          color: StyleSheet.WHITE,
+          fontWeight: FontWeight.w300
+        ),
+      ),
+    );
+  }
+
+  Widget get _horizontalInfoBar {
+
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: _SHEET_BORDER_RADIUS, vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          _basicInfo("Prep", "15 min"),
+          _basicInfo("Cook", "10 min"),
+          _basicInfo("Serves", "4-6")
+        ],
+      ),
+    );
+  }
+
+  Widget _basicInfo(String title, String data) {
+
+    return Column(
+      children: <Widget> [
+        Text(
+          title.toUpperCase(),
+          style: TextStyle(
+            color: StyleSheet.WHITE,
+            fontSize: _INFO_SIZE
+          ),
+        ),
+        Text(
+          data.toUpperCase(),
+          style: TextStyle(
+            color: StyleSheet.WHITE,
+            fontSize: _INFO_SIZE - 1,
+            fontWeight: FontWeight.w300
+          ),
+        ),
+      ]
+    );
+  }
+
+  Widget get tabBody {
+
+    return Consumer<TabNavigationModel> (
+      builder: (context, model, _) {
+        Widget contents = ingredientsList;
+        if(model.currentTab == 1) {
+          contents = directionsList;
+        }
+        return GestureDetector(
+          onHorizontalDragEnd: (DragEndDetails details) => onHorizontalSwipe(details, model),
+          child: ClipPath(
+            clipper: TabClipper(),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+              child: Stack(
+                children: <Widget> [
+                  Container(color: Colors.white12),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      TabBox(title: "Ingredients", index: 0),
+                      TabBox(title: "Directions", index: 1),
+                    ],
+                  ),
+                  Positioned(
+                    top: TabClipper.TAB_HEIGHT,
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                        top: 20,
+                        left: TabClipper.tabSpacing(MediaQuery.of(context).size.width),
+                        right: TabClipper.tabSpacing(MediaQuery.of(context).size.width),
+                      ),
+                      child: contents,
+                    ),
+                  )
+                ]
+              )
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget get ingredientsList {
+    return Text(
+      '– 3 large brown eggs\n– 1 cup of Wild Rice\n– 2 ½ cups of Kale\n– 1 sweet potato',
+      style: TextStyle(
+        color: StyleSheet.WHITE,
+        fontWeight: FontWeight.w300,
+        fontSize: 20,
+        height: 2
+      ),
+    );
+  }
+
+  Widget get directionsList {
+    return Text(
+      '– 1 of something\n– 2 cups of something else\n– 3 tbsp of the last thing',
+      style: TextStyle(
+        color: StyleSheet.WHITE,
+        fontWeight: FontWeight.w300,
+        fontSize: 20,
+        height: 1.5
+      ),
+    );
+  }
+
+  void onHorizontalSwipe(DragEndDetails details, TabNavigationModel model) {
+
+    if(details.primaryVelocity < 0) {
+      model.currentTab++;
+    } else if(details.primaryVelocity > 0) {
+      model.currentTab--;
+    }
+  }
+}
+
+class TabBox extends StatelessWidget {
+
+  final String title;
+  final int index;
+
+  TabBox({@required this.title, @required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+
+    double screenWidth = MediaQuery.of(context).size.width;
+    
+    return Consumer<TabNavigationModel>(
+      builder: (context, model, _) {
+        bool enabled = model.currentTab == index;
+        return Button(
+          onPressed: () {
+            model.currentTab = index;
+          },
+          child:
+          Container(
+            alignment: Alignment.center,
+            width: TabClipper.tabSize(screenWidth) / 2,
+            height: TabClipper.TAB_HEIGHT,
+            child: Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                AnimatedOpacity(
+                  opacity: enabled ? 0 : 0.06,
+                  duration: Duration(milliseconds: 200),
+                  child: Container(  // Tab indicator
+                    color: Colors.black,
+                    height: TabClipper.TAB_HEIGHT,
+                    width: screenWidth / 2,
+                  ),
+                ),
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 200),
+                  height: enabled ? 16 : 15,
+                  child: FittedBox(
+                    fit: BoxFit.fitHeight,
+                    child: AnimatedOpacity(
+                      opacity: enabled ? 1 : 0.5,
+                      duration: Duration(milliseconds: 200),
+                      child: Text(
+                        title.toUpperCase(),
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        );
+      },
+    );
+  }
+}
+
+class TabClipper extends CustomClipper<Path> {
+
+  static const double TAB_HEIGHT = 50;
+  static const double TAB_RADIUS = 30;
+  static const double TAB_SIZE_FACTOR = 0.85;
+
+  static double tabSize(double screenWidth) => screenWidth * TAB_SIZE_FACTOR;
+  static double tabSpacing(double screenWidth) => (screenWidth - tabSize(screenWidth))/2;
+
+  @override
+  Path getClip(Size size) {
+
+    double width = tabSize(size.width);
+
+    final path = Path()
+      ..addRRect(RRect.fromRectAndRadius(
+          Rect.fromLTWH((size.width - width)/2, 0, width, size.height),
+          const Radius.circular(TAB_RADIUS)
+        )
+      )..addRect(Rect.fromLTWH(0, TAB_HEIGHT, size.width, size.height - TAB_HEIGHT))
+      ..close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(TabClipper oldClipper) => false;
+}
